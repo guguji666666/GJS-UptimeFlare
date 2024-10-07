8341ff819f17185d3ec67bc1d31a98f494b3f4c3
咕咕鸡 <gucaoji@gmail.com>
2024-10-07 22:50:08 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -1,107 +1,140 @@
 const pageConfig = {
-  // Title for your status page
-  title: "AM科技's Status Page",
-  // Links shown at the header of your status page, could set `highlight` to `true`
-  links: [
-    { link: 'https://am.809098.xyz', label: '个人博客', highlight: true },
-    { link: 'https://youtube.com/@AM_CLUB', label: 'AM科技' },
-    { link: 'https://github.com/amclubs', label: 'GitHub' },
-    { link: 'https://809098.xyz', label: 'Blog'},
-  ],
-}
-
-const workerConfig = {
-  // Write KV at most every 3 minutes unless the status changed
-  kvWriteCooldownMinutes: 3,
-  // Enable HTTP Basic auth for status page & API by uncommenting the line below, format `<USERNAME>:<PASSWORD>`
-  // passwordProtection: 'username:password',
-  // Define all your monitors here
-  monitors: [
-    // Example HTTP Monitor
-    {
-      // `id` should be unique, history will be kept if the `id` remains constant
-      id: 'am.809098.xyz',
-      // `name` is used at status page and callback message
-      name: '个人博客',
-      // `method` should be a valid HTTP Method
-      method: 'GET',
-      // `target` is a valid URL
-      target: 'https://am.809098.xyz',
-      // [OPTIONAL] `tooltip` is ONLY used at status page to show a tooltip
-      tooltip: 'This is a tooltip for this monitor',
-      // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
-      statusPageLink: 'https://am.809098.xyz',
-      // [OPTIONAL] `expectedCodes` is an array of acceptable HTTP response codes, if not specified, default to 2xx
-      // expectedCodes: [200],
-      // [OPTIONAL] `timeout` in millisecond, if not specified, default to 10000
-      timeout: 10000,
-      // [OPTIONAL] headers to be sent
-      // headers: {
-      //   'User-Agent': 'Uptimeflare',
-      //   Authorization: 'Bearer YOUR_TOKEN_HERE',
-      // },
-      // [OPTIONAL] body to be sent
-      // body: 'Hello, world!',
-      // [OPTIONAL] if specified, the response must contains the keyword to be considered as operational.
-      // responseKeyword: 'success',
-      // [OPTIONAL] if specified, the check will run in your specified region,
-      // refer to docs https://github.com/lyc8503/UptimeFlare/wiki/Geo-specific-checks-setup before setting this value
-      // checkLocationWorkerRoute: 'https://am.809098.xyz',
+    // Title for your status page
+    title: "咕咕鸡 Status Page",
+    // Links shown at the header of your status page, could set `highlight` to `true`
+    links: [
+      //{ link: 'https://am.809098.xyz', label: '个人博客', highlight: true },
+      //{ link: 'https://youtube.com/@AM_CLUB', label: 'AM科技' },
+      { link: 'https://github.com/guguji666666', label: 'GitHub' },
+      { link: 'https://809098.xyz', label: 'AM科技'},      
+    ],
+  }
+  
+  const workerConfig = {
+    // Write KV at most every 3 minutes unless the status changed
+    kvWriteCooldownMinutes: 3,
+    // Enable HTTP Basic auth for status page & API by uncommenting the line below, format `<USERNAME>:<PASSWORD>`
+    // passwordProtection: 'username:password',
+    // Define all your monitors here
+    monitors: [
+      // Example HTTP Monitor
+      {
+        // `id` should be unique, history will be kept if the `id` remains constant
+        id: 'nezhakk.guguji.us.kg',
+        // `name` is used at status page and callback message
+        name: '哪吒面板',
+        // `method` should be a valid HTTP Method
+        method: 'GET',
+        // `target` is a valid URL
+        target: 'https://nezhakk.guguji.us.kg/',
+        // [OPTIONAL] `tooltip` is ONLY used at status page to show a tooltip
+        tooltip: 'This is a tooltip for this monitor',
+        // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
+        statusPageLink: 'https://nezhakk.guguji.us.kg/',
+        // [OPTIONAL] `expectedCodes` is an array of acceptable HTTP response codes, if not specified, default to 2xx
+        // expectedCodes: [200],
+        // [OPTIONAL] `timeout` in millisecond, if not specified, default to 10000
+        timeout: 10000,
+        // [OPTIONAL] headers to be sent
+        // headers: {
+        //   'User-Agent': 'Uptimeflare',
+        //   Authorization: 'Bearer YOUR_TOKEN_HERE',
+        // },
+        // [OPTIONAL] body to be sent
+        // body: 'Hello, world!',
+        // [OPTIONAL] if specified, the response must contains the keyword to be considered as operational.
+        // responseKeyword: 'success',
+        // [OPTIONAL] if specified, the check will run in your specified region,
+        // refer to docs https://github.com/lyc8503/UptimeFlare/wiki/Geo-specific-checks-setup before setting this value
+        // checkLocationWorkerRoute: 'https://am.809098.xyz',
+      },
+      // Example TCP Monitor
+      {
+        id: 'rp.guguji.us.kg',
+        name: 'NPM_MS600_gugu-docker-prod',
+        // `method` should be `TCP_PING` for tcp monitors
+        method: 'GET',
+        // `target` should be `host:port` for tcp monitors
+        target: 'hhttps://rp.guguji.us.kg/',
+        tooltip: 'My production server monitor',
+        statusPageLink: 'https://rp.guguji.us.kg/',
+        timeout: 10000,
+      },
+      {
+        id: 'img.tigaultraman.com',
+        name: '大盘鸡tigaultraman图床',
+        // `method` should be `TCP_PING` for tcp monitors
+        method: 'GET',
+        // `target` should be `host:port` for tcp monitors
+        target: 'https://img.tigaultraman.com/',
+        tooltip: 'My production server monitor',
+        statusPageLink: 'https://img.tigaultraman.com/',
+        timeout: 10000,
+      },
+      {
+        id: 'image.gulaoda.com',
+        name: '绿联gulaoda图床',
+        // `method` should be `TCP_PING` for tcp monitors
+        method: 'GET',
+        // `target` should be `host:port` for tcp monitors
+        target: 'https://image.gulaoda.com/',
+        tooltip: 'My production server monitor',
+        statusPageLink: 'https://image.gulaoda.com/',
+        timeout: 10000,
+      },
+      {
+        id: 'jump.tigaultraman.com',
+        name: '堡垒机内',
+        // `method` should be `TCP_PING` for tcp monitors
+        method: 'GET',
+        // `target` should be `host:port` for tcp monitors
+        target: 'https://jump.tigaultraman.com/',
+        tooltip: 'My production server monitor',
+        statusPageLink: 'https://jump.tigaultraman.com/',
+        timeout: 10000,
+      }    
+    ],
+    notification: {
+      // [Optional] apprise API server URL
+      // if not specified, no notification will be sent
+      appriseApiServer: "https://apprise.example.com/notify",
+      // [Optional] recipient URL for apprise, refer to https://github.com/caronc/apprise
+      // if not specified, no notification will be sent
+      recipientUrl: "tgram://bottoken/ChatID",
+      // [Optional] timezone used in notification messages, default to "Etc/GMT"
+      timeZone: "Asia/Shanghai",
+      // [Optional] grace period in minutes before sending a notification
+      // notification will be sent only if the monitor is down for N continuous checks after the initial failure
+      // if not specified, notification will be sent immediately
+      gracePeriod: 5,
     },
-    // Example TCP Monitor
-    {
-      id: '809098.xyz',
-      name: 'Blog',
-      // `method` should be `TCP_PING` for tcp monitors
-      method: 'GET',
-      // `target` should be `host:port` for tcp monitors
-      target: 'https://809098.xyz',
-      tooltip: 'My production server monitor',
-      statusPageLink: 'https://809098.xyz',
-      timeout: 10000,
+    callbacks: {
+      onStatusChange: async (
+        env: any,
+        monitor: any,
+        isUp: boolean,
+        timeIncidentStart: number,
+        timeNow: number,
+        reason: string
+      ) => {
+        // This callback will be called when there's a status change for any monitor
+        // Write any Typescript code here
+  
+        // This will not follow the grace period settings and will be called immediately when the status changes
+        // You need to handle the grace period manually if you want to implement it
+      },
+      onIncident: async (
+        env: any,
+        monitor: any,
+        timeIncidentStart: number,
+        timeNow: number,
+        reason: string
+      ) => {
+        // This callback will be called EVERY 1 MINTUE if there's an on-going incident for any monitor
+        // Write any Typescript code here
+      },
     },
-  ],
-  notification: {
-    // [Optional] apprise API server URL
-    // if not specified, no notification will be sent
-    appriseApiServer: "https://apprise.example.com/notify",
-    // [Optional] recipient URL for apprise, refer to https://github.com/caronc/apprise
-    // if not specified, no notification will be sent
-    recipientUrl: "tgram://bottoken/ChatID",
-    // [Optional] timezone used in notification messages, default to "Etc/GMT"
-    timeZone: "Asia/Shanghai",
-    // [Optional] grace period in minutes before sending a notification
-    // notification will be sent only if the monitor is down for N continuous checks after the initial failure
-    // if not specified, notification will be sent immediately
-    gracePeriod: 5,
-  },
-  callbacks: {
-    onStatusChange: async (
-      env: any,
-      monitor: any,
-      isUp: boolean,
-      timeIncidentStart: number,
-      timeNow: number,
-      reason: string
-    ) => {
-      // This callback will be called when there's a status change for any monitor
-      // Write any Typescript code here
-
-      // This will not follow the grace period settings and will be called immediately when the status changes
-      // You need to handle the grace period manually if you want to implement it
-    },
-    onIncident: async (
-      env: any,
-      monitor: any,
-      timeIncidentStart: number,
-      timeNow: number,
-      reason: string
-    ) => {
-      // This callback will be called EVERY 1 MINTUE if there's an on-going incident for any monitor
-      // Write any Typescript code here
-    },
-  },
-}
-
-// Don't forget this, otherwise compilation fails.
-export { pageConfig, workerConfig }
+  }
+  
+  // Don't forget this, otherwise compilation fails.
+  export { pageConfig, workerConfig }
